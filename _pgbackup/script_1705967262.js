c8a7c383713e7583d2f4dbca17911b17f0ac8646
Naswade21 <naswade21@gmail.com>
2024-01-22 18:49:13 -0500

    Declared Const in JS
    Declared the const of the project that are connected to the id's. The next step is to declare functions and write what they do in ENGLISH.

---
+++ _pgbackup/script_1705967262.js
@@ -0,0 +1,4 @@
+const buttonCheck = document.getElementById('check-btn');
+
+const result = document.querySelectorAll('#result');
+
---
+++ script.js
@@ -0,0 +1,5 @@
+const palindromeChecker = document.getElementById('check-btn');
+const palindromeResult = document.getElementById('#result');
+const output = document.getElementById('output');
+const palindromeTextInput = document.getElementById('text-input');
+
