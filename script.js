5ffd3682f52801a779bfecffae334ff35f2d2205
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2024-02-08 16:00:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const palindromeResult = document.getElementById('#result');
 const palindromeTextInput = document.getElementById('text-input');
 let isError = false;
 
-const isPalindrome = (str) => {
-    const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, ''); //Makes words all lowercase then replaces non-alphanumeric characters(^) with the string the user types
+function isPalindrome(str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, ''); //Makes words all lowercase then replaces non-alphanumeric characters(^) with the string the user types
     let reverseStr = cleanStr.split().reverse().join(); //Take your cleaned input and split individually, reverse the words and join them back together to see if its similar backwards and forwards
-
     return cleanStr === reverseStr; //Checks if forward string and backward string are equal
 }
 
-function paliCheck () {
-    const checkText = isPalindrome(palindromeTextInput.value);
-    palindromeResult.textContent = `${checkText}`
+function insertAnswer() {
+    let ansText = " is a Palindrome";
+    let wrongText = " is not a Palindrome"
+    if (isPalindrome(palindromeTextInput.value) === true) {
+        palindromeResult.innerHTML += `${palindromeTextInput.value}${ansText}`
+    } else {
+         palindromeResult.innerHTML += `${palindromeTextInput.value}${wrongText}`
+    }
 }
 
-palindromeChecker.addEventListener('click', paliCheck);
+palindromeChecker.onclick = function () {
+
+    
+    
+}
